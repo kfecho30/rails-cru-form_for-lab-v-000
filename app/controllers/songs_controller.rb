baf5654149477bc25370b92e8c9f5a9082f09b0b
Kelsey Fecho <kfecho30@gmail.com>
2018-04-30 14:11:18 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,34 @@
 class SongsController < ApplicationController
+  
+    def index
+      @songs = Song.all
+    end
+
+    def show
+      @song = Song.find(params[:id])
+    end
+
+    def new
+      @song = Genre.new
+    end
+
+    def create
+      @song = Genre.create(list_params)
+      redirect_to genre_url(@genre)
+    end
+
+    def edit
+      @song = Genre.find(params[:id])
+    end
+
+    def update
+      @song = Genre.find(params[:id])
+      @song.update(list_params)
+      redirect_to genres_url
+    end
+
+    private
+    def list_params
+      params.require(:song).permit(:name, :artist_id, :genre_id)
+    end
 end
